3cb8d603ef75c73474b77b596ab6682a1c0cd535
Hol1kgmg <abdevs.horisho@icloud.com>
2025-08-24 00:45:57 +0900

    feat:topページからhealthとserver healthへの遷移ボタンを実装

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -5,7 +6,15 @@ export default function Home() {
 		<div className="space-y-16">
 			<h1 className="font-bold text-4xl">Hello World</h1>
 			<p className="text-lg">This is a test</p>
-			<Button>Click me</Button>
+
+			<div className="flex items-center gap-5">
+				<Link href="/health">
+					<Button>Check Health</Button>
+				</Link>
+				<Link href="/serverHealth">
+					<Button>Check SeverHealth</Button>
+				</Link>
+			</div>
 		</div>
 	);
 }
